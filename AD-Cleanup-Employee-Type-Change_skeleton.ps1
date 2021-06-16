131dd5fc384f5ed0d7c495cf424d29b67052e68c
PowershellJunkie <86009671+PowershellJunkie@users.noreply.github.com>
2021-06-16 11:31:32 -0400

    Add files via upload

---
+++ AD-Cleanup-Employee-Type-Change_skeleton.ps1
@@ -0,0 +1,25 @@
+﻿<#
+Get the input for the Department Number and the desired Job Code here. The way our AD is layed out, the $JCode will be used twice; 
+Once to determine the sub-OU that the user(s) in question are located and again to actually change their 'employeetype' AD attribute
+#>
+
+$JCode = Read-Host "Please input Job code desired"
+$OU1 = Read-Host "Input OU here"
+$OU2 = Read-Host "Input Sub-OU here"
+$OU3 = Read-Host "Input sub-sub-OU here"
+
+# Build the array of users based on the input given
+
+$usrArray = @()
+$usrdeets = Get-ADUser -SearchBase "OU=$JCode,OU=$OU3,OU=$OU2,OU=$OU1,DC=tbc,DC=local" -Filter * | Select-Object sAMAccountName
+$usrArray += $usrdeets
+
+# Loop through the array and change the 'employeetype' to the desired Job Code
+
+$usrArray | ForEach-Object{
+
+$user = $_.sAMAccountName
+
+Set-ADUser -Identity $user -Replace @{employeetype=$JCode}
+
+}
